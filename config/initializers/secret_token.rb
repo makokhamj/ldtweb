b56b6196cf2d6b17a33861edd142221b1a543eb2
makokha <makokha@simba.(none)>
2011-04-28 22:05:47 -0700

    webspace

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ldt::Application.config.secret_token = '0f8b46c975e325adc42ef46aa4b12aa8b0dd62b36c3c6eeda7e87c604ceda4c031353bf5f3d303c5f48b21811ad7b106fe7547e1b3fdd2de9650973064823f05'
